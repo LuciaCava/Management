af17731ec040183629db6a9fc6c6acc2e627b2f6
LuciaCava <57954872+LuciaCava@users.noreply.github.com>
2021-04-21 12:26:19 +0200

    Add files via upload
    Add graphs and further measurements

--- Lab1/fixed codes/1server_scenario.py
+++ Lab1/fixed codes/1server_scenario.py
@@ -1,6 +1,7 @@
 import random
 import simpy
-
+import matplotlib.pyplot as plt 
+import numpy as np
 
 # ******************************************************************************
 # To take the measurements
@@ -13,6 +14,7 @@ class Measure:
         self.oldT = OldTimeEvent
         self.delay = AverageDelay
         self.pCloud = SentToCloud
+        self.waitingTime = []
  
 # ******************************************************************************
 # Client
@@ -28,17 +30,18 @@ class Client:
 # ******************************************************************************
 LOAD=0.85
 SERVICE = 10.0 # av service time
-ARRIVAL   = SERVICE/LOAD # av inter-arrival time
+ARRIVAL = SERVICE/LOAD # av inter-arrival time
 TYPE1 = 1 
 
 SIM_TIME = 500000
-maxBuffer = 3 #if we set equal to 1 it is like other case, 1 service and no buffer 
+maxBuffer = 0 #if we set equal to 1 it is like other case, 1 service and no buffer 
 arrivals=0
 users=0
 BusyServer=False # True: server is currently busy; False: server is currently idle
 MM1=[]
 infiniteBuff = False
-
+entrance_user = []
+served_user = []
 
 
 # ******************************************************************************
@@ -48,6 +51,7 @@ fin = input("Do you want to simulate a finite or infinite buffer? Press f for fi
 if fin=='i':
     infiniteBuff = True
 elif fin == 'f':
+    print(1)
     maxBuffer = int(input("Insert the buffer size:\nf"))
     maxBuffer = maxBuffer + 1 
     print("Max number of packets in the systems is = ", maxBuffer)
@@ -59,7 +63,7 @@ elif fin == 'f':
 # ******************************************************************************
 
 # arrivals *********************************************************************
-def arrival_process(environment,queue):
+def arrival_process(environment,queue, service, arrival):
     global users
     global BusyServer
     while True:
@@ -69,7 +73,7 @@ def arrival_process(environment,queue):
         data.ut += users*(environment.now-data.oldT)
         data.oldT = environment.now
         # sample the time until the next event
-        inter_arrival = random.expovariate(1.0/ARRIVAL)
+        inter_arrival = random.expovariate(1.0/arrival)
         
         # check if the user wants to simulate a finite or infinite buffer 
         if infiniteBuff == True: 
@@ -77,10 +81,11 @@ def arrival_process(environment,queue):
             users += 1
             cl=Client(TYPE1,env.now)
             queue.append(cl)
+            entrance_user.append(env.now)
             if users == 1: 
                 BusyServer = True
-                service_time = random.expovariate(1.0/SERVICE)
-                env.process(departure_process(env, service_time,queue))
+                service_time = random.expovariate(1.0/service)
+                env.process(departure_process(env, service_time,queue, service))
         else: 
             #update state variable and put the client in the queue
             # check size of the queue, if it hasn't reach the max capacity yet then we can add the new client in the queue
@@ -89,10 +94,11 @@ def arrival_process(environment,queue):
                 users += 1
                 cl=Client(TYPE1,env.now)
                 queue.append(cl)
+                entrance_user.append(env.now)
                 if users == 1: 
                     BusyServer = True
-                    service_time = random.expovariate(1.0/SERVICE)
-                    env.process(departure_process(env, service_time,queue))
+                    service_time = random.expovariate(1.0/service)
+                    env.process(departure_process(env, service_time,queue, service))
             elif users>=maxBuffer:
                 data.pCloud += 1
         
@@ -105,11 +111,11 @@ def arrival_process(environment,queue):
 # ******************************************************************************
 
 # departures *******************************************************************
-def departure_process(environment, service_time, queue):
+def departure_process(environment, service_time, queue, service):
     global users
     global BusyServer
 
-    
+    served_user.append(env.now)
     yield environment.timeout(service_time)
     #print("Departure no. ",data.dep+1," at time ",environment.now," with ",users," users" )
     
@@ -127,19 +133,17 @@ def departure_process(environment, service_time, queue):
     if users==0: 
         BusyServer = False
     else:
-        service_time = random.expovariate(1.0/SERVICE)
-        env.process(departure_process(env, service_time,queue))
+        service_time = random.expovariate(1.0/service)
+        env.process(departure_process(env, service_time,queue, service))
         # the execution flow will resume here
         # when the "timeout" event is executed by the "environment"
        
-
 # ******************************************************************************
 
 
 # ******************************************************************************
 # the main body of the simulation
 # ******************************************************************************
-
 
 # create the environment
 
@@ -150,11 +154,15 @@ data = Measure(0,0,0,0,0,0)
 env = simpy.Environment()
 
 # start the arrival processes
-env.process(arrival_process(env, MM1))
+env.process(arrival_process(env, MM1, SERVICE, ARRIVAL))
 
 
 # simulate until SIM_TIME
 env.run(until=SIM_TIME)
+
+# compute time in waiting line for each user
+for i in range(len(served_user)-1):
+    data.waitingTime.append(served_user[i] - entrance_user[i])
 
 
 # print output data
@@ -172,9 +180,220 @@ print("Sent to the cloud rate = ", data.pCloud/env.now)
 
 print("\nAverage number of users (E[N]) = ",data.ut/env.now)
 print("Average delay (E[T]) =  ",data.delay/data.dep)
+print("Average time in the waiting line (E[T_w]) =  ", np.mean(data.waitingTime))
 
 
 if len(MM1)>0:
     print("Arrival time of the last element in the queue:",MM1[len(MM1)-1].Tarr)
+
+ 
+  
+# ******************************************************************************
+# PLOTS MEASUREMENTS UNDER DIFFERENT SERVICE RATES 
+# ******************************************************************************
+
+# performance analysis for infinite or no buffer scenario
+if infiniteBuff==True or maxBuffer==1: 
+    #service_rate =np.arange(8, 15, 0.5)  # av service time
+    loads=np.arange(0.05, 1.05, 0.05)
+    ser_rate = []
+    e_n = [] 
+    e_t = []
+    cloud = [] 
+    # simulate the system 
+    #for serv in service_rate: 
+    for load in loads: 
+        if load == 0:
+            pass
+        else:
+            MM1 = [] 
+            arrivals=0
+            users=0
+            BusyServer=False
+            random.seed(42)
+            arrival = SERVICE/load
+            
+            data = Measure(0,0,0,0,0,0)
+            #ser_rate.append(1/serv)
+            env = simpy.Environment()
+            
+            # start the arrival processes
+            env.process(arrival_process(env, MM1, SERVICE, arrival))
+            # simulate until SIM_TIME
+            env.run(until=SIM_TIME)
+            e_n.append(data.ut/env.now)
+            e_t.append(data.delay/data.dep)
+            if infiniteBuff==False:
+                cloud.append(data.pCloud/data.arr)
+    
+    # plot the avg number of packets 
+    plt.plot(loads, e_n, 'r')
+    plt.xlabel('Service rate-loads')
+    plt.ylabel('E[N]')
+    plt.title('Average Number of Users')
+    plt.show()
+    
+    # plot the avg delay
+    plt.plot(loads, e_t, 'r')
+    plt.xlabel('Service rate-loads')
+    plt.ylabel('E[T]')
+    plt.title('Average delay')
+    plt.show()
+    
+    if infiniteBuff==False: 
+        plt.plot(loads, cloud, 'r')
+        plt.xlabel('Service rate-loads')
+        plt.ylabel('Cloud rate')
+        plt.title('Rate of packets sent to the cloud')
+        plt.show()
+    
+
+
+# for finite buffer scenario: plot measurement with diff buffer size 
+if maxBuffer >1 : 
+    loads=np.arange(0.0, 1.05, 0.05)
+    s = len(loads)
+    buffSize = [8,16,32,64]
+    b = len(buffSize)
+    e_n = np.zeros((b,s))
+    e_t = np.zeros((b,s))
+    cloud = np.zeros((b,s))
+    i=0
+    
+    for buff in buffSize:
+        j=0
+        maxBuffer = buff
+        for load in loads: 
+            if load == 0:
+                pass
+            else:
+                MM1 = [] 
+                arrivals=0
+                users=0
+                BusyServer=False
+                random.seed(42)
+                arrival = SERVICE/load
+                
+                data = Measure(0,0,0,0,0,0)
+                env = simpy.Environment()
+                # start the arrival processes
+                env.process(arrival_process(env, MM1, SERVICE, arrival))
+                # simulate until SIM_TIME
+                env.run(until=SIM_TIME)
+                
+                e_n[i,j]= data.ut/env.now
+                e_t[i,j] = data.delay/data.dep
+                cloud[i,j] = data.pCloud/data.arr
+            j = j+1
+            
+        i = i +1
+
+    plt.plot(loads, e_n[0,:], 'r', label='B=8')
+    plt.plot(loads, e_n[1,:], 'b',label='B=16')    
+    plt.plot(loads, e_n[2,:], 'g', label='B=32') 
+    plt.plot(loads, e_n[3,:], 'y', label='B=64')
+    plt.xlabel('Service rate-loads')
+    plt.ylabel('E[N]')
+    plt.title('Average Number of Users')
+    plt.legend()
+    plt.show()
+    
+    plt.plot(loads, e_t[0,:], 'r', label='B=8')
+    plt.plot(loads, e_t[1,:], 'b',label='B=16')    
+    plt.plot(loads, e_t[2,:], 'g', label='B=32') 
+    plt.plot(loads, e_t[3,:], 'y', label='B=64')
+    plt.xlabel('LOads')
+    plt.ylabel('E[T]')
+    plt.legend()
+    plt.title('Average delay')
+    plt.show()
+    
+    plt.plot(loads, cloud[0,:], 'r', label='B=8')
+    plt.plot(loads, cloud[1,:], 'b',label='B=16')    
+    plt.plot(loads, cloud[2,:], 'g', label='B=32') 
+    plt.plot(loads, cloud[3,:], 'y', label='B=64')
+    plt.xlabel('loads')
+    plt.ylabel('Cloud rate')
+    plt.title('Rate of packets sent to the cloud')
+    plt.legend()
+    plt.show()
+            
+            
+ # ******************************************************************************
+# ANALYZE CASE NO BUFFER DIFFERENCE SERVICE RATES
+# ******************************************************************************
+if infiniteBuff==False and maxBuffer==1:
+    print("DIFFERENT SERVICE RATE")
+    service = [5,7,10,15]
+    b = len(service)
+    loads=np.arange(0.05, 1.05, 0.05)
+    s = len(loads)
+    e_n = np.zeros((b,s))
+    e_t = np.zeros((b,s))
+    cloud = np.zeros((b,s))
+    i=0
+    for serv in service:
+        j=0
+        for load in loads: 
+            if load == 0:
+                pass
+            else:
+                MM1 = [] 
+                arrivals=0
+                users=0
+                BusyServer=False
+                random.seed(42)
+                arrival = serv/load
+                
+                data = Measure(0,0,0,0,0,0)
+                env = simpy.Environment()
+                # start the arrival processes
+                env.process(arrival_process(env, MM1, SERVICE, arrival))
+                # simulate until SIM_TIME
+                env.run(until=SIM_TIME)
+                
+                e_n[i,j]= data.ut/env.now
+                e_t[i,j] = data.delay/data.dep
+                cloud[i,j] = data.pCloud/data.arr
+            j = j+1
+            
+        i = i +1
+
+    plt.plot(loads, e_n[0,:], 'r', label='serv')
+    plt.plot(loads, e_n[1,:], 'b',label='serv')    
+    plt.plot(loads, e_n[2,:], 'g', label='B=32') 
+    plt.plot(loads, e_n[3,:], 'y', label='B=64')
+    plt.xlabel('Service rate-loads')
+    plt.ylabel('E[N]')
+    plt.title('Average Number of Users')
+    plt.legend()
+    plt.show()
+    
+    plt.plot(loads, e_t[0,:], 'r', label='B=8')
+    plt.plot(loads, e_t[1,:], 'b',label='B=16')    
+    plt.plot(loads, e_t[2,:], 'g', label='B=32') 
+    plt.plot(loads, e_t[3,:], 'y', label='B=64')
+    plt.xlabel('Service rate-loads')
+    plt.ylabel('E[T]')
+    plt.legend()
+    plt.title('Average delay')
+    plt.show()
+    
+    plt.plot(loads, cloud[0,:], 'r', label='B=8')
+    plt.plot(loads, cloud[1,:], 'b',label='B=16')    
+    plt.plot(loads, cloud[2,:], 'g', label='B=32') 
+    plt.plot(loads, cloud[3,:], 'y', label='B=64')
+    plt.xlabel('Service rate-loads')
+    plt.ylabel('Cloud rate')
+    plt.title('Rate of packets sent to the cloud')
+    plt.legend()
+    plt.show()
+            
+            
+            
+            
+            
+            
+            
     
 
--- Lab1/fixed codes/2servers_scenario.py
+++ Lab1/fixed codes/2servers_scenario.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 import random
 import simpy
-
+import matplotlib.pyplot as plt 
+import numpy as np
 
 # ******************************************************************************
 # To take the measurements
@@ -14,11 +15,13 @@ class Measure_system:
         self.oldT = OldTimeEvent
         self.delay = AverageDelay
         self.pCloud = SentToCloud
+        self.waitingTime = []
         
       
 class Measure_queue:
-    def __init__(self, Narr):
+    def __init__(self, Narr, busy):
         self.arr = Narr
+        self.busy = 0
         
        
 # ******************************************************************************
@@ -28,8 +31,6 @@ class Client:
     def __init__(self,Type,ArrivalT):
         self.type = Type
         self.Tarr = ArrivalT
-    def print(self):
-        return self.type
         
 
 # ******************************************************************************
@@ -39,7 +40,7 @@ LOAD=0.85
 SERVICE = 10.0 # av service time
 ARRIVAL   = SERVICE/LOAD # av inter-arrival time
 TYPE1 = 1 
-maxBuffer = 4 
+maxBuffer = 0 
 SIM_TIME = 500000
 
 arrivals=0
@@ -50,7 +51,8 @@ BusyServer2=False # True: server is currently busy; False: server is currently i
 queue =[] 
 MM2=[]
 infiniteBuff = False
-
+entrance_user = []
+served_user = []
 
 
 # ******************************************************************************
@@ -71,7 +73,7 @@ elif fin == 'f':
 # ******************************************************************************
 
 # arrivals *********************************************************************
-def arrival_process(environment,queue):
+def arrival_process(environment,queue, serv, arrival):
     global users
     global BusyServer1, BusyServer2
     
@@ -84,36 +86,41 @@ def arrival_process(environment,queue):
         dataSys.oldT = environment.now
             
         # sample the time until the next event
-        inter_arrival = random.expovariate(1.0/ARRIVAL)
+        inter_arrival = random.expovariate(1.0/arrival)
         
         if infiniteBuff==True:
             users += 1
             cl=Client(TYPE1,env.now)
             queue.append(cl)
+            entrance_user.append(env.now)
             if BusyServer1 == False: 
                 BusyServer1 = True
-                service_time1 = random.expovariate(1.0/SERVICE)
-                env.process(departure_process1(env, service_time1,queue))
+                service_time1 = random.expovariate(1.0/serv)
+                data1.busy += service_time1
+                env.process(departure_process1(env, service_time1,queue,serv))
             elif BusyServer2 == False:
                 BusyServer2 = True
-                service_time2 = random.expovariate(1.0/SERVICE)
-                env.process(departure_process2(env, service_time2,queue))
+                service_time2 = random.expovariate(1.0/serv)
+                data2.busy += service_time2
+                env.process(departure_process2(env, service_time2,queue,serv))
             
         else: 
             if users<maxBuffer:
                 users += 1
                 cl=Client(TYPE1,env.now)
                 queue.append(cl)
-                
+                entrance_user.append(env.now)
                 # distinguish the two queues
                 if BusyServer1 == False: 
                     BusyServer1 = True
-                    service_time1 = random.expovariate(1.0/SERVICE)
-                    env.process(departure_process1(env, service_time1,queue))
+                    service_time1 = random.expovariate(1.0/serv)
+                    data1.busy += service_time1
+                    env.process(departure_process1(env, service_time1,queue, serv))
                 elif BusyServer2 == False:
                     BusyServer2 = True
-                    service_time2 = random.expovariate(1.0/SERVICE)
-                    env.process(departure_process2(env, service_time2,queue))
+                    service_time2 = random.expovariate(1.0/serv)
+                    data2.busy += service_time2
+                    env.process(departure_process2(env, service_time2,queue, serv))
             elif users>=maxBuffer:
                 dataSys.pCloud+=1
             
@@ -124,11 +131,11 @@ def arrival_process(environment,queue):
 # ******************************************************************************
 
 # departures *******************************************************************
-def departure_process1(environment, service_time,queue):
+def departure_process1(environment, service_time,queue, serv):
     global users
     global BusyServer1, BusyServer2
     
-    
+    served_user.append(env.now)
     yield environment.timeout(service_time)
     #print("Departure no. ",data.dep+1," at time ",environment.now," with ",users," users" )
     data1.arr +=1 
@@ -143,17 +150,19 @@ def departure_process1(environment, service_time,queue):
     if len(queue)==1 or len(queue)==0: 
         BusyServer1 = False
     else:
-        service_time = random.expovariate(1.0/SERVICE)
-        env.process(departure_process1(env, service_time,queue))
+        service_time = random.expovariate(1.0/serv)
+        data1.busy += service_time
+        env.process(departure_process1(env, service_time,queue,serv))
 
         # the execution flow will resume here
         # when the "timeout" event is executed by the "environment"
        
      
-def departure_process2(environment, service_time,queue):
+def departure_process2(environment, service_time,queue, serv):
     global users
     global BusyServer1, BusyServer2
 
+    served_user.append(env.now)
     yield environment.timeout(service_time)
     
     #print("Departure no. ",data.dep+1," at time ",environment.now," with ",users," users" )
@@ -171,8 +180,9 @@ def departure_process2(environment, service_time,queue):
     if len(queue)==1 or len(queue)==0: 
         BusyServer2 = False
     else:
-        service_time = random.expovariate(1.0/SERVICE)
-        env.process(departure_process2(env, service_time,queue))
+        service_time = random.expovariate(1.0/serv)
+        data2.busy += service_time
+        env.process(departure_process2(env, service_time,queue,serv))
 
         # the execution flow will resume here
         # when the "timeout" event is executed by the "environment"
@@ -190,17 +200,21 @@ def departure_process2(environment, service_time,queue):
 random.seed(42)
 
 dataSys = Measure_system(0,0,0,0,0,0)
-data1 = Measure_queue(0)
-data2 = Measure_queue(0)
+data1 = Measure_queue(0,0)
+data2 = Measure_queue(0,0)
 
 env = simpy.Environment()
 
 # start the arrival processes
-env.process(arrival_process(env,MM2))
+env.process(arrival_process(env,MM2, SERVICE, ARRIVAL))
 
 
 # simulate until SIM_TIME
 env.run(until=SIM_TIME)
+
+# compute time in waiting line for each user
+for i in range(len(served_user)-1):
+    dataSys.waitingTime.append(served_user[i] - entrance_user[i])
 
 
 # print output data
@@ -218,10 +232,154 @@ print("Sent to the cloud rate = ", dataSys.pCloud/env.now)
 
 print("\nAverage number of users (E[N]) = ",dataSys.ut/env.now)
 print("Average delay (E[T]) =  ",dataSys.delay/dataSys.dep)
+print("Average time in the waiting line (E[T_w]) =  ", np.mean(dataSys.waitingTime))
 
-
+print("\nThe first server processed packets: ", data1.arr)
+print(f"and was busy the {(data1.busy/env.now)*100} % of the time")
+print(data1.busy)
+print("\nThe second server processed packets: ", data2.arr)
+print(f"and was busy the {(data2.busy/env.now)*100} % of the time")
 
 if len(MM2)>0:
     print("Arrival time of the last element in the queue:",MM2[len(MM2)-1].Tarr)
     
+    
+# ******************************************************************************
+# PLOTS MEASUREMENTS UNDER DIFFERENT SERVICE RATES 
+# ******************************************************************************
+
+# performance analysis for infinite or no buffer scenario
+if infiniteBuff==True or maxBuffer==2: 
+    #service_rate =np.arange(8, 15, 0.5)  # av service time
+    loads=np.arange(0.05, 1.05, 0.05)
+    ser_rate = []
+    e_n = [] 
+    e_t = []
+    cloud = [] 
+    
+    # simulate the system 
+    #for serv in service_rate: 
+    for load in loads: 
+        if load == 0:
+            pass
+        else:
+            MM2 = [] 
+            arrivals=0
+            users=0
+            
+            BusyServer1=False # True: server is currently busy; False: server is currently idle
+            BusyServer2=False
+            random.seed(42)
+            arrival = SERVICE/load
+            
+            dataSys = Measure_system(0,0,0,0,0,0)
+            data1 = Measure_queue(0,0)
+            data2 = Measure_queue(0,0)
+            #ser_rate.append(1/serv)
+            env = simpy.Environment()
+            
+            # start the arrival processes
+            env.process(arrival_process(env, MM2, SERVICE, arrival))
+            # simulate until SIM_TIME
+            env.run(until=SIM_TIME)
+            e_n.append(dataSys.ut/env.now)
+            
+            e_t.append(dataSys.delay/dataSys.dep)
+            if infiniteBuff==False:
+                cloud.append(dataSys.pCloud/dataSys.arr)
+    
+    # plot the avg number of packets 
+    plt.plot(loads, e_n, 'r')
+    plt.xlabel('Service rate-loads')
+    plt.ylabel('E[N]')
+    plt.title('Average Number of Users')
+    plt.show()
+    
+    # plot the avg delay
+    plt.plot(loads, e_t, 'r')
+    plt.xlabel('Service rate-loads')
+    plt.ylabel('E[T]')
+    plt.title('Average delay')
+    plt.show()
+    
+    if infiniteBuff==False: 
+        plt.plot(loads, cloud, 'r')
+        plt.xlabel('Service rate-loads')
+        plt.ylabel('Cloud rate')
+        plt.title('Rate of packets sent to the cloud')
+        plt.show()
+   
+# for finite buffer scenario: plot measurement with diff buffer size 
+if maxBuffer>2 : 
+    print('boh')
+    loads=np.arange(0.05, 1.05, 0.05)
+    s = len(loads)
+    #buffSize = [8,16,32,64] # with values used in the case of one server, no big changes 
+    buffSize = [4,8,16]
+    b = len(buffSize)
+    e_n = np.zeros((b,s))
+    e_t = np.zeros((b,s))
+    cloud = np.zeros((b,s))
+    i=0
+    
+    for buff in buffSize:
+        j=0
+        maxBuffer = buff
+        print(buff)
+        for load in loads: 
+            if load == 0:
+                pass
+            else:
+                MM2 = [] 
+                arrivals=0
+                users=0
+                BusyServer1=False # True: server is currently busy; False: server is currently idle
+                BusyServer2=False
+                
+                random.seed(42)
+                arrival = SERVICE/load
+                
+                dataSys = Measure_system(0,0,0,0,0,0)
+                env = simpy.Environment()
+                # start the arrival processes
+                env.process(arrival_process(env, MM2, SERVICE, arrival))
+                # simulate until SIM_TIME
+                env.run(until=SIM_TIME)
+                
+                e_n[i,j]= dataSys.ut/env.now
+                e_t[i,j] = dataSys.delay/dataSys.dep
+                cloud[i,j] = dataSys.pCloud/dataSys.arr
+            j = j+1
+            
+        i = i +1
+
+    plt.plot(loads, e_n[0,:], 'r', label='B=8')
+    plt.plot(loads, e_n[1,:], 'b',label='B=16')    
+    plt.plot(loads, e_n[2,:], 'g', label='B=32') 
+     #plt.plot(loads, e_n[3,:], 'y', label='B=64')
+    plt.xlabel('Service rate-loads')
+    plt.ylabel('E[N]')
+    plt.title('Average Number of Users')
+    plt.legend()
+    plt.show()
+    
+    plt.plot(loads, e_t[0,:], 'r', label='B=8')
+    plt.plot(loads, e_t[1,:], 'b',label='B=16')    
+    plt.plot(loads, e_t[2,:], 'g', label='B=32') 
+    #plt.plot(loads, e_t[3,:], 'y', label='B=64')
+    plt.xlabel('Service rate-loads')
+    plt.ylabel('E[T]')
+    plt.legend()
+    plt.title('Average delay')
+    plt.show()
+    
+    plt.plot(loads, cloud[0,:], 'r', label='B=8')
+    plt.plot(loads, cloud[1,:], 'b',label='B=16')    
+    plt.plot(loads, cloud[2,:], 'g', label='B=32') 
+    #plt.plot(loads, cloud[3,:], 'y', label='B=64')
+    plt.xlabel('Service rate-loads')
+    plt.ylabel('Cloud rate')
+    plt.title('Rate of packets sent to the cloud')
+    plt.legend()
+    plt.show()
 
